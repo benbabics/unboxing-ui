c4d54a036e4571d508b783d88ac77e139f6adc94
Ben Babics <bbabics@gmail.com>
2020-08-12 14:38:25 -0400

    clean up

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -5,97 +5,59 @@ import { AuthService } from 'app/core/auth/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-    selector     : 'auth-sign-in',
-    templateUrl  : './sign-in.component.html',
-    styleUrls    : ['./sign-in.component.scss'],
+    selector: 'auth-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    animations   : TreoAnimations
+    animations: TreoAnimations
 })
-export class AuthSignInComponent implements OnInit
-{
-    signInForm: FormGroup;
-    message: any;
+export class AuthSignInComponent implements OnInit {
 
-    /**
-     * Constructor
-     *
-     * @param {ActivatedRoute} _activatedRoute
-     * @param {AuthService} _authService
-     * @param {FormBuilder} _formBuilder
-     * @param {Router} _router
-     */
-    constructor(
-        private _activatedRoute: ActivatedRoute,
-        private _authService: AuthService,
-        private _formBuilder: FormBuilder,
-        private _router: Router
-    )
-    {
-        // Set the defaults
-        this.message = null;
-    }
+  signInForm: FormGroup;
+  message: any;
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _authService: AuthService,
+    private _formBuilder: FormBuilder,
+    private _router: Router
+  ) {
+    this.message = null;
+  }
 
-    /**
-     * On init
-     */
-    ngOnInit(): void
-    {
-        // Create the form
-        this.signInForm = this._formBuilder.group({
-            email     : ['watkins.andrew@company.com'],
-            password  : ['admin'],
-            rememberMe: ['']
-        });
-    }
+  ngOnInit(): void {
+    this.signInForm = this._formBuilder.group({
+      email:      [ 'watkins.andrew@company.com' ],
+      password:   [ 'admin' ],
+      rememberMe: [ '' ],
+    });
+  }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Public methods
-    // -----------------------------------------------------------------------------------------------------
+  signIn(): void {
+    this.signInForm.disable();
+    this.message = null;
 
-    /**
-     * Sign in
-     */
-    signIn(): void
-    {
-        // Disable the form
-        this.signInForm.disable();
+    const credentials = this.signInForm.value;
+    this._authService.signIn(credentials).subscribe(
+      ()          => this.handleSigninSuccess(),
+      ({ error }) => this.handleSigninFailure(error)
+    );
+  }
 
-        // Hide the message
-        this.message = null;
+  private handleSigninSuccess(): void {
+    const redirectURL = this._activatedRoute.snapshot.queryParamMap.get('redirectURL') || '/signed-in-redirect';
+    this._router.navigateByUrl(redirectURL);
+  }
 
-        // Get the credentials
-        const credentials = this.signInForm.value;
+  private handleSigninFailure(error: any): void {
+    this.signInForm.enable();
 
-        // Sign in
-        this._authService.signIn(credentials)
-            .subscribe(() => {
-
-                // Set the redirect url.
-                // The '/signed-in-redirect' is a dummy url to catch the request and redirect the user
-                // to the correct page after a successful sign in. This way, that url can be set via
-                // routing file and we don't have to touch here.
-                const redirectURL = this._activatedRoute.snapshot.queryParamMap.get('redirectURL') || '/signed-in-redirect';
-
-                // Navigate to the redirect url
-                this._router.navigateByUrl(redirectURL);
-
-            }, (response) => {
-
-                // Re-enable the form
-                this.signInForm.enable();
-
-                // Show the error message
-                this.message = {
-                    appearance: 'outline',
-                    content   : response.error,
-                    shake     : true,
-                    showIcon  : false,
-                    type      : 'error'
-                };
-            });
-    }
+    this.message = {
+      type:       'error',
+      appearance: 'outline',
+      content:    error,
+      shake:      true,
+      showIcon:   false,
+    };
+  }
 }
