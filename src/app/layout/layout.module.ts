9297d9661a59d9f113733ba1f7a5d580cbd24c62
Ben Babics <bbabics@gmail.com>
2020-08-12 23:11:40 -0400

    Simplify layouts to “empty” and “futuristic”

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,39 +1,9 @@
 import { NgModule } from '@angular/core';
-import { TreoDrawerModule } from '@treo/components/drawer';
 import { LayoutComponent } from 'app/layout/layout.component';
 import { EmptyLayoutModule } from 'app/layout/layouts/empty/empty.module';
-import { CenteredLayoutModule } from 'app/layout/layouts/horizontal/centered/centered.module';
-import { EnterpriseLayoutModule } from 'app/layout/layouts/horizontal/enterprise/enterprise.module';
-import { MaterialLayoutModule } from 'app/layout/layouts/horizontal/material/material.module';
-import { ModernLayoutModule } from 'app/layout/layouts/horizontal/modern/modern.module';
-import { BasicLayoutModule } from 'app/layout/layouts/vertical/basic/basic.module';
-import { ClassicLayoutModule } from 'app/layout/layouts/vertical/classic/classic.module';
-import { ClassyLayoutModule } from 'app/layout/layouts/vertical/classy/classy.module';
-import { CompactLayoutModule } from 'app/layout/layouts/vertical/compact/compact.module';
-import { DenseLayoutModule } from 'app/layout/layouts/vertical/dense/dense.module';
-import { FuturisticLayoutModule } from 'app/layout/layouts/vertical/futuristic/futuristic.module';
-import { ThinLayoutModule } from 'app/layout/layouts/vertical/thin/thin.module';
+import { FuturisticLayoutModule } from 'app/layout/layouts/futuristic/futuristic.module';
+
 import { SharedModule } from 'app/shared/shared.module';
-
-const modules = [
-  // Empty
-  EmptyLayoutModule,
-
-  // Horizontal navigation
-  CenteredLayoutModule,
-  EnterpriseLayoutModule,
-  MaterialLayoutModule,
-  ModernLayoutModule,
-
-  // Vertical navigation
-  BasicLayoutModule,
-  ClassicLayoutModule,
-  ClassyLayoutModule,
-  CompactLayoutModule,
-  DenseLayoutModule,
-  FuturisticLayoutModule,
-  ThinLayoutModule
-];
 
 @NgModule({
   declarations: [
@@ -41,10 +11,12 @@ const modules = [
   ],
   imports: [
     SharedModule,
-    ...modules
+    EmptyLayoutModule,
+    FuturisticLayoutModule,
   ],
   exports: [
-    ...modules
+    EmptyLayoutModule,
+    FuturisticLayoutModule,
   ]
 })
 export class LayoutModule { }
