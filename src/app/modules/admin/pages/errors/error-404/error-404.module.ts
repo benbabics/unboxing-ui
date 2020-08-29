f7992848afc3931fd2fa28277f95b44fee3d1677
Ben Babics <bbabics@gmail.com>
2020-08-29 16:06:30 -0400

    Update UI for error-404

--- src/app/modules/admin/pages/errors/error-404/error-404.module.ts
+++ src/app/modules/admin/pages/errors/error-404/error-404.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from 'app/modules/admin/pages/errors/error-404/error-404.component';
 import { error404Routes } from 'app/modules/admin/pages/errors/error-404/error-404.routing';
 
 @NgModule({
-    declarations: [
-        Error404Component
-    ],
-    imports     : [
-        RouterModule.forChild(error404Routes)
-    ]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forChild( error404Routes ),
+  ],
+  declarations: [
+    Error404Component,
+  ],
 })
-export class Error404Module
-{
-}
+export class Error404Module { }
