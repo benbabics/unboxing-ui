80ecc0116ff6e7692f260d8f303ce517be98fa09
Ben Babics <bbabics@gmail.com>
2020-09-01 14:54:42 -0400

    Addition of PendingChangesGuard

---
+++ projects/lib-common/src/lib/guards/pending-changes/pending-changes.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+export interface ComponentCanDeactivate {
+  canDeactivate: () => boolean | Observable<boolean> | Promise<boolean>;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
+
+  canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> | Promise<boolean> {
+    return component.canDeactivate();
+  }
+}
