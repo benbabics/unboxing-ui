ab8997d1af83669dffcef390baee49bb641d153b
Ben Babics <bbabics@gmail.com>
2020-09-12 15:21:35 -0400

    ProjectFilters functionality with QueryParams & NGXS Form state

--- src/app/modules/admin/apps/projects/components/project-filters/project-filters.component.ts
+++ src/app/modules/admin/apps/projects/components/project-filters/project-filters.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { toPairs, set } from 'lodash';
+import { Subject } from 'rxjs';
+import { map, takeUntil, tap } from 'rxjs/operators';
+import { Store, Actions, ofActionSuccessful } from '@ngxs/store';
+import { UpdateFormValue } from '@ngxs/form-plugin';
+import { SearchProject } from './../../states';
+import { plainToFlattenObject } from '../../../../../../../../projects/lib-common/src/public-api';
 
 @Component({
   selector: 'project-filters',
   templateUrl: './project-filters.component.html',
   styleUrls: ['./project-filters.component.scss']
 })
-export class ProjectFiltersComponent implements OnInit {
+export class ProjectFiltersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private _destroy$ = new Subject();
+  
+  projectFiltersForm: FormGroup;
+  
+  constructor(
+    actions$: Actions,
+    private _store: Store,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+  ) {
+    this._buildForm();
+    
+    // on SearchProject.Search, update the URL queryParams
+    actions$.pipe(
+      takeUntil( this._destroy$ ),
+      ofActionSuccessful( SearchProject.Search ),
+      map(({ payload }) => plainToFlattenObject( payload )),
+      map(queryParams => ({ queryParams, queryParamsHandling: 'merge' })),
+      tap(queryParams => _router.navigate( [], <any>queryParams )),
+    )
+    .subscribe();
   }
 
+  ngOnInit() {
+    // on queryParam updates, update ngxs form state
+    this._activatedRoute.queryParams.pipe(
+      takeUntil( this._destroy$ ),
+      map(params => toPairs( params )),
+      map(pairs  => pairs.reduce((model, [k,v]) => set(model,k,v), {})),
+      tap(value  => this._store.dispatch(
+        new UpdateFormValue({ value, path: 'searchProject.projectFiltersForm' })
+      )),
+    )
+    .subscribe();
+  }
+
+  ngOnDestroy() {
+    this._destroy$.next( true );
+    this._destroy$.complete();
+  }
+
+  handleSubmit(): void {
+    const filters = this.projectFiltersForm.getRawValue();
+    this._store.dispatch( new SearchProject.Search(filters) );
+  }
+
+  private _buildForm(): void {
+    this.projectFiltersForm = new FormGroup({
+      brand: new FormGroup({
+        id: new FormControl( '' ),
+      }),
+
+      project: new FormGroup({
+        title: new FormControl( '' ),
+        slug:  new FormControl( '' ),
+      }),
+
+      users: new FormGroup({ }),
+    });
+  }
 }
