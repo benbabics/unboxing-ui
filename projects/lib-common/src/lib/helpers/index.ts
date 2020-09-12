3726c6430a94be20e92757b10dcaaf2277a4b5d4
Ben Babics <bbabics@gmail.com>
2020-09-12 15:19:59 -0400

    Addition of helper: plainToFlattenObject

--- projects/lib-common/src/lib/helpers/index.ts
+++ projects/lib-common/src/lib/helpers/index.ts
@@ -1,2 +1,26 @@
+import { forEach, isObject } from 'lodash';
+
+
 export * from './carousel/carousel';
 export * from './stack/stack';
+
+
+export function plainToFlattenObject(object) {
+  const result = {};
+
+  function flatten(obj, prefix = "") {
+    forEach(obj, (value, key) => {
+      if ( !value ) return;
+
+      if ( isObject(value) ) {
+        flatten( value, `${ prefix }${ key }.` );
+      } 
+      else {
+        result[ `${ prefix }${ key }` ] = value;
+      }
+    })
+  }
+
+  flatten( object );
+  return result;
+}
