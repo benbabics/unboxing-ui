b6c2dc7ded78f5af835c8e1451301d879eba2859
Ben Babics <bbabics@gmail.com>
2020-11-02 08:51:40 -0500

    Refactored EditorChangeHistoryService
    catalog is now a private member that is asisgned the value on changes to history (Map) to avoid unnecessary calculations; potential performance gain.
    
    Added _reset$ EventEmitter to know when the history has been reset so the member changes is notified and reassigns to the latest history (Map) value.

--- src/app/modules/admin/apps/projects/services/editor-change-history/editor-change-history.service.ts
+++ src/app/modules/admin/apps/projects/services/editor-change-history/editor-change-history.service.ts
@@ -1,7 +1,8 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Actions, Store } from '@ngxs/store';
 import { EntityActionType, ofEntityActionSuccessful, UpdateActive } from '@ngxs-labs/entity-state';
-import { debounceTime, filter, tap, withLatestFrom } from 'rxjs/operators';
+import { merge } from "rxjs";
+import { debounceTime, filter, flatMap, tap, withLatestFrom } from 'rxjs/operators';
 import { Slide, SlideState, Stack } from '@projects/lib-common/src/public-api';
 
 @Injectable({
@@ -10,17 +11,14 @@ import { Slide, SlideState, Stack } from '@projects/lib-common/src/public-api';
 export class EditorChangeHistoryService {
 
   private _history: Map<string, Stack>;
+  private _reset$ = new EventEmitter();
 
   changes: Stack;
+  catalog: { id: string, changes: Stack }[] = [];
   onChange: EventEmitter<void> = new EventEmitter();
 
   get totalOfChanges(): number {
     return this.catalog.reduce((n, { changes }) => n + changes.index, 0);
-  }
-  
-  get catalog() {
-    return [ ...this._history.entries() ]
-      .map(([ id, changes ]) => ({ id, changes }));
   }
 
   get hasPrevious(): boolean {
@@ -32,25 +30,26 @@ export class EditorChangeHistoryService {
   
   constructor(
     actions$: Actions,
-    private store: Store,
+    private _store: Store,
   ) {
     this._history = new Map();
     this.reset();
 
-    store.select( SlideState.active )
-      .pipe(
-        filter(slide => !!slide),
-        tap(({ id }) => this.changes = this._history.get( `${ id }` )), // TODO: deserialize payload ids to string
-      )
-      .subscribe();
+    merge(
+      this._reset$,
+      _store.select( SlideState.active ).pipe( filter(slide => !!slide) ),
+    )
+    .pipe( flatMap(() => _store.select(SlideState.activeId)) )
+    .subscribe(id => this.changes = this._history.get( `${ id }` )); // TODO: deserialize payload ids to string
 
     actions$
       .pipe(
         ofEntityActionSuccessful( SlideState, EntityActionType.UpdateActive ),
-        withLatestFrom( store.select(SlideState.activeId) ),
+        withLatestFrom( _store.select(SlideState.activeId) ),
         debounceTime( 250 ),
         filter(([{ payload }]) => payload != this.changes.item),
         tap(([{ payload }, id]) => this._history.get( `${ id }` ).add( payload )), // TODO: deserialize payload ids to string
+        tap(() => this._assignCatalog()),
       )
       .subscribe(() => this.onChange.emit());
   }
@@ -58,24 +57,32 @@ export class EditorChangeHistoryService {
   reset(): void {
     this._history.clear();
 
-    const slides = this.store.selectSnapshot( SlideState.entitiesMap );
+    const slides = this._store.selectSnapshot( SlideState.entitiesMap );
     Object.keys( slides ).forEach(key => {
       const { attributes } = slides[ key ];
       this._history.set(key, new Stack([{ attributes }]));
     });
+
+    this._assignCatalog();
+    this._reset$.emit();
   }
 
   undo(): void {
     this.changes.previous();
-    this.updateActiveSlide( this.changes.item );
+    this._updateActiveSlide( this.changes.item );
   }
 
   redo(): void {
     this.changes.next();
-    this.updateActiveSlide( this.changes.item );
+    this._updateActiveSlide( this.changes.item );
   }
 
-  private updateActiveSlide(slide: Slide): void {
-    this.store.dispatch( new UpdateActive(SlideState, slide) );
+  private _updateActiveSlide(slide: Slide): void {
+    this._store.dispatch( new UpdateActive(SlideState, slide) );
+  }
+
+  private _assignCatalog(): void {
+    this.catalog = [ ...this._history.entries() ]
+      .map(([ id, changes ]) => ({ id, changes }));
   }
 }
