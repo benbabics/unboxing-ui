5090cc631b19912989e82eb043cce679c6dfe8c8
Ben Babics <bbabics@gmail.com>
2020-11-10 16:49:49 -0500

    UI for AssetFinder in ProjectShow

--- src/app/modules/admin/apps/projects/components/assets/asset-finder/asset-finder.component.ts
+++ src/app/modules/admin/apps/projects/components/assets/asset-finder/asset-finder.component.ts
@@ -27,44 +27,44 @@ export class AssetFinderComponent implements OnInit, OnDestroy {
   @Output() onSelectIterationIds = new EventEmitter();
   
   constructor(
-    private store: Store,
+    private _store: Store,
   ) { }
 
   ngOnInit() {
     this._directoryId$.pipe(
       takeUntil(this._destroy$),
-      tap(dirId => this.mapToActiveDirectory(dirId)),
-      tap(dirId => this.mapToDirectories(dirId)),
-      tap(dirId => this.mapToElements(dirId)),
+      tap(dirId => this.mapToActiveDirectory( dirId )),
+      tap(dirId => this.mapToDirectories( dirId )),
+      tap(dirId => this.mapToElements( dirId )),
     )
     .subscribe();
     
-    this.setActiveDirectory(null);
+    this.setActiveDirectory( null );
   }
 
   ngOnDestroy() {
-    this._destroy$.next(true);
+    this._destroy$.next( true );
     this._destroy$.unsubscribe();
   }
 
   directoryDetails(id: string): any {
-    const isIterationId = id => this.iterationIds.includes(id);
-    const ancestors = this.store.selectSnapshot(AssetDirectoryState.ancestors(id)).map(dir => dir.id);
+    const isIterationId = id => this.iterationIds.includes( id );
+    const ancestors = this._store.selectSnapshot( AssetDirectoryState.ancestors(id) ).map(dir => dir.id);
 
     return {
       id,
-      isChecked:  [ ...ancestors, id ].some(isIterationId),
-      isDisabled: ancestors.some(isIterationId),
+      isChecked:  [ ...ancestors, id ].some( isIterationId ),
+      isDisabled: ancestors.some( isIterationId ),
     }
   }
 
   isDirectoryIdSelected(id: string): boolean {
-    const ancestors = this.store.selectSnapshot(AssetDirectoryState.ancestors(id));
-    return ancestors.some(dir => this.iterationIds.includes(dir.id));
+    const ancestors = this._store.selectSnapshot( AssetDirectoryState.ancestors(id) );
+    return ancestors.some(dir => this.iterationIds.includes( dir.id ));
   }
 
   handleNavigateDirectory(id: string): void {
-    this.setActiveDirectory(id);
+    this.setActiveDirectory( id );
   }
 
   handleSelectDirectory(id: string): void {
@@ -72,25 +72,25 @@ export class AssetFinderComponent implements OnInit, OnDestroy {
   }
   
   private setActiveDirectory(id: string): void {
-    this._directoryId$.next(id);
+    this._directoryId$.next( id );
   }
 
   private mapToActiveDirectory(dirId: string): void {
-    const dirs = this.store.selectSnapshot(AssetDirectoryState.entities);
-    this.activeDirectory = find(dirs, ['id', dirId]);
+    const dirs = this._store.selectSnapshot( AssetDirectoryState.entities );
+    this.activeDirectory = find(dirs, [ 'id', dirId ]);
   }
 
   private mapToDirectories(dirId: string): void {
-    this.directories = this.store.selectSnapshot(
-      AssetDirectoryState.descendants(dirId)
+    this.directories = this._store.selectSnapshot(
+      AssetDirectoryState.descendants( dirId )
     );
   }
 
   private mapToElements(dirId: string): void {
-    const els = this.store.selectSnapshot(AssetElementState.descendants(dirId));
+    const els = this._store.selectSnapshot( AssetElementState.descendants(dirId) );
 
     if (this.entityType !== AssetElementFormat.All) {
-      this.elements = filter(els, ['format', this.entityType]);
+      this.elements = filter(els, [ 'format', this.entityType ]);
     }
     else {
       this.elements = els;
