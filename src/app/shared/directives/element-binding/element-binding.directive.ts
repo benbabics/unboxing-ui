fee6849a1b2d744ebd2519354b8fad090d55deed
Ben Babics <bbabics@gmail.com>
2020-11-18 18:30:51 -0500

    Addition of tooltip menu for ElementMenu Directive using tippy.js!
    gh-13

--- src/app/shared/directives/element-binding/element-binding.directive.ts
+++ src/app/shared/directives/element-binding/element-binding.directive.ts
@@ -1,23 +1,27 @@
 import { Directive, ElementRef, HostListener, Input, OnInit, OnDestroy, Renderer2 } from '@angular/core';
 import { Actions, ofActionDispatched, Store } from '@ngxs/store';
-import { Slide } from '@projects/lib-common/src/lib/states';
 import { isNil } from 'lodash';
 import { merge, Subject } from 'rxjs';
 import { delay, map, takeUntil, tap } from 'rxjs/operators';
+import { Slide } from '@projects/lib-common/src/lib/states';
+import { NgTippyService, Instance } from 'angular-tippy';
 
 @Directive({
   selector: '[elementBinding]'
 })
-export class ElementBindingDirective implements OnDestroy {
+export class ElementBindingDirective implements OnInit, OnDestroy {
 
-  private _destroy$ = new Subject();
+  private _destroy$  = new Subject();
+  private _tooltip: Instance;
 
   @Input() elementBinding: string;
 
   @HostListener( 'click' ) 
   onClick() {
     // separate thread to resolve "tap" issues 
-    setTimeout(() => this._store.dispatch( new Slide.FocusElement(this.elementBinding) ));
+    setTimeout(() => this._store.dispatch( 
+      new Slide.FocusElement( this.elementBinding )
+    ));
   }
   
   @HostListener( 'mouseenter' ) 
@@ -34,6 +38,7 @@ export class ElementBindingDirective implements OnDestroy {
     private _store: Store,
     private _elementRef: ElementRef,
     private _renderer: Renderer2,
+    private _tippy: NgTippyService,
   ) {
     merge(
       actions$.pipe( ofActionDispatched(Slide.FocusElement) ),
@@ -41,12 +46,24 @@ export class ElementBindingDirective implements OnDestroy {
     )
     .pipe(
       takeUntil( this._destroy$ ),
+      delay( 0 ),
       map(({ name }) => name === this.elementBinding),
-      delay( 100 ),
       tap(isActive => this.toggle( 'binding-is-active', isActive )),
-      tap(() => this.handleFocusElement())
+      tap(isActive => this.renderTooltip( isActive )),
+      tap(()       => this.handleFocusElement())
     )
     .subscribe();
+  }
+
+  ngOnInit() {
+    this._tooltip = this._tippy.init(this._elementRef, {
+      arrow:       true,
+      allowHTML:   true,
+      interactive: true,
+      interactiveBorder: 30,
+      theme: "light-border",
+      content: `<strong>Bolded content</strong>`,
+    });
   }
 
   ngOnDestroy() {
@@ -61,7 +78,24 @@ export class ElementBindingDirective implements OnDestroy {
     this._renderer[ isActive ? "addClass" : "removeClass" ]( this._elementRef.nativeElement, cname );
   }
 
+  renderTooltip( isActive: boolean) {
+    if ( isActive ) {
+      this._tooltip.show();
+      this._tooltip.set({ trigger: "manual" });
+      this._tooltip.disable();
+    }
+    else {
+      this._tooltip.hide();
+      this._tooltip.set({ trigger: "mouseenter focus" });
+      this._tooltip.enable();
+    }
+  }
+
   handleFocusElement(): void {
-    this._elementRef.nativeElement.scrollIntoViewIfNeeded({ block: 'end', inline: 'end', behavior: 'smooth' });
+    this._elementRef.nativeElement.scrollIntoViewIfNeeded({
+      block:    "end",
+      inline:   "end",
+      behavior: "smooth",
+    });
   }
 }
