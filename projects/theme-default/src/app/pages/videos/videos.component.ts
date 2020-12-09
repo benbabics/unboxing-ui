32d30169838875557b3fd44368868409e138f083
Ben Babics <bbabics@gmail.com>
2020-12-09 18:00:53 -0500

    Initial markup & functionality for photos page

--- projects/theme-default/src/app/pages/videos/videos.component.ts
+++ projects/theme-default/src/app/pages/videos/videos.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
+import { Slide, SlideState } from '@projects/lib-common/src/lib/states';
+import { AssetService } from '@projects/theme-default/src/app/services';
+import { Carousel } from '@projects/theme-default/src/app/services/carousel/carousel/carousel';
 
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.scss']
 })
-export class VideosComponent implements OnInit {
+export class VideosComponent implements OnDestroy {
 
-  constructor() { }
+  private _destroy$: Subject<boolean> = new Subject();
+  carousel: Carousel;
 
-  ngOnInit(): void {
+  @Select( SlideState.active ) slide$: Observable<Slide>;
+
+  constructor(
+    asset: AssetService,
+  ) {
+    asset.videos$
+      .pipe(
+        takeUntil( this._destroy$ ),
+        tap(videos => this.carousel = new Carousel( videos )),
+      )
+      .subscribe();
   }
 
+  ngOnDestroy() {
+    this._destroy$.next( true );
+    this._destroy$.unsubscribe();
+  }
+
+  handlePreviousClick(): void {
+    this.carousel.previous();
+  }
+
+  handleNextClick(): void {
+    this.carousel.next();
+  }
+
+  handleSetSlide( slide ): void {
+    this.carousel.setBySlide( slide );
+  }
 }
